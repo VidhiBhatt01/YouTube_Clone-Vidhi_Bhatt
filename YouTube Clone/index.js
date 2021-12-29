e516f6cfaef9885c779184e7a5933aea43054dfe
VIDHI BHATT <83345228+VidhiBhatt01@users.noreply.github.com>
2021-12-29 18:16:05 +0530

    Update index.js

--- YouTube Clone/index.js
+++ YouTube Clone/index.js
@@ -6,3 +6,16 @@ console.log(sidebar);
 menu.addEventListener('click', function () 
   {sidebar.classList.toggle('show-sidebar');
 });
+
+//for search bar
+const searchInput = document.querySelector('.search-bar');
+const searchBtn = document.querySelector('.search-btn');
+
+let searchLink = "https://www.youtube.com/results?search_query=";
+
+searchBtn.addEventListener('click', () => {
+   if(searchInput.value.length){
+     location.href = searchLink + searchInput.value;
+     console.log(searchInput);
+   }
+})
